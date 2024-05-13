15773e3624153340b6934298fce4a292d9aa16e8
Massimo Larger <massimoalberto.larger@alumnos.ulagos.cl>
2024-05-13 17:31:24 -0400

    Actualizacion procesor coneccion base de datos

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Execute")
+import "dotenv/config";
+import express from "express";
+import todoRoute from "./routes/todo.route.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/todos", todoRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
