05eb41fad54467db2423e23885b3170ec1479acd
Massimo Larger <massimoalberto.larger@alumnos.ulagos.cl>
2024-05-13 18:43:19 -0400

    actualizacion errores

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,6 @@ import { todoController } from "../controllers/controller.js";
 
 const router = Router();
 
-router.get("/", todoController.getAll);
-router.post("/", todoController.create);
+router.get("/", todoController.getAll);}
 
 export default router;
