415b2df990e16958ec5d0ea2426f47124a654a95
Massimo Larger <massimoalberto.larger@alumnos.ulagos.cl>
2024-06-17 12:13:13 -0400

    Verificación patentes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import Ppu from './ppu.js';
 
 dotenv.config();
 
@@ -22,7 +23,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
-// Ruta para realizar una consulta
+// Existing routes
 app.get('/api/consultau', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM usuario');
@@ -33,7 +34,6 @@ app.get('/api/consultau', async (req, res) => {
   }
 });
 
-// Ruta para realizar una consulta
 app.get('/api/consultav', async (req, res) => {
   try {
     const result = await pool.query(`
@@ -48,14 +48,13 @@ app.get('/api/consultav', async (req, res) => {
   }
 });
 
-// Ruta para realizar una consulta
 app.get('/api/consultar', async (req, res) => {
   try {
     const result = await pool.query(`
         SELECT u.nombre as usuario, r.id_vehiculo as patente, v.descripcion as modelo
         FROM registra r
-        JOIN usuario u ON r.ID_Usuario = u.Telefono
-        JOIN vehiculo v ON r.ID_Vehiculo = v.Patente;
+        JOIN usuario u ON u.rut = r.rut
+        JOIN vehiculo v ON r.id_vehiculo = v.patente;
     `);
     res.json(result.rows);
   } catch (err) {
@@ -64,67 +63,23 @@ app.get('/api/consultar', async (req, res) => {
   }
 });
 
-// Nueva ruta para verificar credenciales
-app.post('/api/login', async (req, res) => {
-  const { telefono, contrasena } = req.body;
+// New route for PPU verification
+app.post('/api/verify-ppu', (req, res) => {
+  const { ppu } = req.body;
 
   try {
-    const result = await pool.query(
-      'SELECT * FROM usuario WHERE telefono = $1 AND contrasena = $2',
-      [telefono, contrasena]
-    );
-
-    if (result.rows.length > 0) {
-      const {telefono} = result.rows[0];
-      res.json({ success: true, userId: telefono, message: 'Usuario encontrado'});
-    } else {
-      res.json({ success: false, message: 'Usuario no encontrado, o algun campo incorrecto' });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-  }
-});
-
-app.post('/api/register', async (req, res) => {
-  const { telefono, nombre, tipo, contrasena } = req.body;
-  
-  try {
-    const result = await pool.query(
-      'INSERT INTO usuario VALUES ($1, $2, $3, $4) RETURNING *',
-      [telefono, nombre, tipo, contrasena]
-    );
-    
-    res.status(200).json({ success: true, id: result.rows[0].id });
+    const ppuInstance = new Ppu(ppu);
+    res.status(200).json({ success: true, ppu: ppuInstance });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Error al registrar usuario.' });
+    res.status(400).json({ success: false, message: error.message });
   }
 });
 
-app.post('/api/vehiculos', async (req, res) => {
-  const { patente, year, model, vehicleId, userId } = req.body;
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-  try {
-    // Insertar el nuevo vehículo
-    const result = await pool.query(
-      'INSERT INTO Vehiculo VALUES ($1, $2, $3, $4) RETURNING *',
-      [patente, year, model, vehicleId]
-    );
-
-    // Registrar la relación del vehículo con el usuario
-    const resultado = await pool.query(
-      'INSERT INTO Registra VALUES ($1, $2) RETURNING *',
-      [userId, patente]
-    );
-
-    res.status(200).send({ success: true, vehicle: result.rows[0].id, register: resultado.rows[0].id });
-  } catch (error) {
-    console.error('Error al registrar el vehículo:', error);
-    res.status(500).send('Error al registrar el vehículo');
-  }
-});
-
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
---
+++ ppu.js
@@ -0,0 +1,146 @@
+const _ = require('lodash');
+const _variables = require('./variables.json');
+const _letterDvDb = require('./letterDvDB.json');
+
+class Ppu {
+  constructor (ppu) {
+    this.ppu = ppu;
+    this.format = this.verifyPpuType(ppu);
+    this.numbers = this.transformInPreNumber(this);
+    this.dv = this.calculateDv(this);
+  }
+
+  verifyPpuType (ppu) {
+    if (ppu.length !== _variables.MAX_LENGTH) {
+      throw new Error("La patente(ppu) ingresada es de largo inválido")
+    }
+
+    // New Moto Format
+    if (this.constructor.isMotoNew(ppu)) {
+      return _variables.motoNewFormat;
+    }
+
+    // Old Moto Format
+    if (this.constructor.isMotoOld(ppu)) {
+      return _variables.motoOldFormat;
+    }
+
+    // New Auto Format
+    if (this.constructor.isAutoMovilNew(ppu)) {
+      return _variables.autoNewFormat;
+    }
+    // Old Auto Format
+    if (this.constructor.isAutoMovilOld(ppu)) {
+      return _variables.autoOldFormat;
+    }
+    // Carro Arrastre Format
+    if (this.constructor.isCarroArrastre(ppu)) {
+      throw new Error("CARRO DE ARRASTRE no es un formato válido.")
+    }
+    throw new Error("Ppu no corresponde a ningun formato válido")
+  }
+
+  static isAutoMovilNew (ppu) {
+    // Validate format XXXX11
+    const regexAutomovil = new RegExp(/^[b-d,f-h,j-l,p,r-t,v-z]{2}[\-\. ]?[b-d,f-h,j-l,p,r-t,v-z]{2}[\.\- ]?[0-9]{2}$/, 'i');
+    return regexAutomovil.test(ppu);
+  }
+
+  static isAutoMovilOld (ppu) {
+    // Validate format XX1111
+    const regexAutomovil = new RegExp(/^[a-z]{2}[\.\- ]?[0-9]{2}[\.\- ]?[0-9]{2}$/, 'i');
+    return regexAutomovil.test(ppu);
+  }
+
+  static isMotoOld (ppu) {
+    // Validate format XX0111 / XX111 / XXX011 / XXX11
+    const regexMoto = new RegExp(/^[a-z]{2}0[0-9]{3}$|^[a-z]{2}[0-9]{3}$/, 'i');
+    return regexMoto.test(ppu);
+  }
+
+  static isMotoNew (ppu) {
+    // Validate format XX0111 / XX111 / XXX011 / XXX11
+    const regexMoto = new RegExp(/^[b-d,f-h,j-l,p,r-t,v-z]{3}[0-9]{2}$|^[b-d,f-h,j-l,p,r-t,v-z]{3}0[0-9]{2}$/, 'i');
+    return regexMoto.test(ppu);
+  }
+
+  static isCarroArrastre (ppu) {
+    // Validate format XXX111
+    const regexCarroArrastre = new RegExp(/^[a-zA-Z]{3}[1-9]{1}[0-9]{2}$/, 'i');
+    return regexCarroArrastre.test(ppu);
+  }
+
+  transformInPreNumber (ppu) {
+    // Reconocer Typo de transformación por tipo de ppu
+    let type = ppu.format.type;
+    // Convertir dependiendo el formato
+    if ('LLL.NNN' === type || 'LLLL.NN' === type) {
+      let ppuToArray = Array.from(ppu.ppu);
+      _.each(ppuToArray, (letter, key) => {
+        if (!Number.isInteger(parseInt(letter))) {
+          ppuToArray[ key ] = parseInt(_letterDvDb[ letter.toUpperCase() ]);
+        } else {
+          ppuToArray[ key ] = parseInt(letter);
+        }
+      });
+      return ppuToArray
+    }
+    if ('LL.NNNN' === type) {
+      let ppuNumbersAsArray;
+      let ppuLetters = ppu.ppu.substring(0, 2);
+      let ppuNumbersASstring = ppu.ppu.substring(2, 6);
+      let ppuLettersTransformedToNumbersASstring = this.lettersToNumbers(ppuLetters);
+      let ppuNumbersAll = ppuLettersTransformedToNumbersASstring + ppuNumbersASstring;
+
+      ppuNumbersAsArray = Array.from(ppuNumbersAll);
+
+      return ppuNumbersAsArray
+    }
+
+  }
+
+  lettersToNumbers (ppuLetters) {
+    let transformedPpuLetters = _letterDvDb[ ppuLetters ]
+    if (typeof transformedPpuLetters === 'string') {
+      return transformedPpuLetters;
+    } else {
+      throw new Error("Letras de Ppu de Auto Antiguo inválidas");
+    }
+  }
+
+  calculateDv (ppu) {
+    // Identificar algoritmo por formato de ppu.
+    let format = ppu.format.type;
+    if (format === 'LL.NNNN') {
+      let S =
+        ppu.numbers[ 6 ] * 2 +
+        ppu.numbers[ 5 ] * 3 +
+        ppu.numbers[ 4 ] * 4 +
+        ppu.numbers[ 3 ] * 5 +
+        ppu.numbers[ 2 ] * 6 +
+        ppu.numbers[ 1 ] * 7 +
+        ppu.numbers[ 0 ] * 2;
+      let Ri = S % 11;
+      let Rf = 11 - Ri;
+      if (Rf === 11) return "0";
+      if (Rf === 10) return "K";
+      if (Rf < 10) return Rf.toString();
+    }
+    if (format === 'LLL.NNN' || format === 'LLLL.NN') {
+      let SP =
+        ppu.numbers[ 5 ] * 2 +
+        ppu.numbers[ 4 ] * 3 +
+        ppu.numbers[ 3 ] * 4 +
+        ppu.numbers[ 2 ] * 5 +
+        ppu.numbers[ 1 ] * 6 +
+        ppu.numbers[ 0 ] * 7;
+      let R = SP % 11;
+      if (R === 0) return "0";
+      let RV = 11 - R;
+      if (RV === 10) return "K";
+      return RV.toString();
+    }
+  }
+}
+
+module.exports = Ppu;
